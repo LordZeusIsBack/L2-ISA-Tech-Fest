fe832b44807ddff0d79df5869f89233046438008
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-02-06 20:29:07 +0530

    corrected the listing iteration issue

--- ai_model/model.py
+++ ai_model/model.py
@@ -13,7 +13,7 @@ class AIModel:
         self.chat_history = []
     
     def start_chat_session(self):
-        return self.model.start_chat(history=[self.chat_history])
+        return self.model.start_chat(history=self.chat_history)
 
     def add_to_history(self, prompt, response):
         self.chat_history.append({"role": "user", "parts": [prompt]})
