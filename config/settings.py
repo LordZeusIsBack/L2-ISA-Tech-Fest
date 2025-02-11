c5e72fae62a4079ae9bde80aff3a5238299f7897
Anubhav Sharma <anubhavsharma5645@gmail.com>
2025-02-12 03:37:45 +0530

    Added Back Security Measures

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,10 @@
 import os
+from dotenv import load_dotenv
 
 class Config:
     def __init__(self):
-        self.API_KEY = os.getenv('AIzaSyC4XsIDXOhn6SPi2tWu57XWBdxvw9cDfCc')
+        load_dotenv()
+        self.API_KEY = os.getenv('GOOGLE_API_KEY')
         self.GENERATION_CONFIGURATION = {
             "temperature": 0.75,
             "top_p": 0.95,
